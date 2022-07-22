6d0be005ed7b6d95a211e5a6e3e497075ca5339b
Md. Sujan Ahmed <shobuj2sujon@gmail.com>
2022-07-23 01:50:00 +0600

    useState remove

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Header from './common/Header/Header'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Pages from './pages/Pages';
